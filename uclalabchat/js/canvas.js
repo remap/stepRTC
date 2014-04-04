b40042d22f122e31bfe57bf9a3c4ad88fa7ce4a4
Alex Nano <nanodust@gmail.com>
2014-04-03 19:59:31 -0700

    Update canvas.js

--- uclalabchat/js/canvas.js
+++ uclalabchat/js/canvas.js
@@ -1,26 +1,23 @@
-
 	
 var v = document.getElementById('remoteVideo');
 //var v = document.getElementById('localVideo');
 var canvas = document.getElementById('c');
 var context = canvas.getContext('2d');
 
-var cw = canvas.clientWidth;//Math.floor(canvas.clientWidth / 100);
-var ch = canvas.clientHeight;//Math.floor(canvas.clientHeight / 100);
+var cw = canvas.clientWidth;
+var ch = canvas.clientHeight;
 
 canvas.width = cw;
 canvas.height = ch;
 
-//v.addEventListener('play', function(){
+
 draw(v,context,cw,ch);
-//},false);
-//},false);
 
 // frequency of blitting video -> canvas
-freq = 3 // ms
+freq = 5 // ms
 
 function draw(v,c,w,h) {
 	console.log("drawing...")
     c.drawImage(v,0,0,w,h);
-    setTimeout(draw,5,v,c,w,h);
+    setTimeout(draw,freq,v,c,w,h);
 }
