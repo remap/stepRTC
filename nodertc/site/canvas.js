a516050164c59c36fa73bdcf1c54d4910ada5793
nano <nanodust@gmail.com>
2014-04-14 11:46:41 -0500

    updated comments

--- nodertc/site/canvas.js
+++ nodertc/site/canvas.js
@@ -1,11 +1,11 @@
+
 function initCanvas(){
-//var v = document.getElementById('remoteVideo');
 var v = document.getElementById('remote' + gSocketId);
-//var v = document.getElementById('localVideo');
 var canvas = document.getElementById('c');
 var context = canvas.getContext('2d');
 
-// this size doesn't really matter - it just has to be *something* other than clientWidth/Height
+// this size below doesn't really matter - 
+// it just has to be *something* other than clientWidth/Height
 // the true size of display is set in css for c element
 
 var cw = 640;//document.body.clientWidth; 
@@ -17,13 +17,10 @@ canvas.height = ch;
 draw(v,context,cw,ch);
 
 }
-
 // frequency of blitting video -> canvas
 freq = 5 //ms
 
 function draw(v,c,w,h) {
-	//gm
-	console.log("drawing...")
     c.drawImage(v,0,0,w,h);
     setTimeout(draw,freq,v,c,w,h);
 }
